781dd0b55ae1f75cdb69a8c86e2edf51f1ddd453
ESTEVAO <estevaorssouza@hotmail.com>
2023-12-23 18:18:36 -0300

    add api deve..

--- src/context/BaseApi.tsx
+++ src/context/BaseApi.tsx
@@ -1,5 +1,7 @@
 import Axios from "axios";
 
 export const BaseApi = Axios.create({
-  baseURL: "http://10.10.10.50:3080/",
+  baseURL: "https://apicardapio.onrender.com/",
+  //baseURL: "http://10.10.10.50:3080/",
+  //producao: "https://apicardapio.onrender.com/",
 });
