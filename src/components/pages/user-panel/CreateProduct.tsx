447c569f7009243d3694ef64effcac890c1d1e26
ESTEVAO <estevaorssouza@hotmail.com>
2023-12-29 15:50:17 -0300

    fix

--- src/components/pages/user-panel/CreateProduct.tsx
+++ src/components/pages/user-panel/CreateProduct.tsx
@@ -7,12 +7,13 @@ import {
     Heading,
     Button,
     VStack,
-    Image,
-    InputGroup,
+    useToast,
     InputRightElement,
+    InputGroup,
+    Image
   } from "@chakra-ui/react";
   import React, { useEffect, useState } from "react";
-  import BreadCrumb from "../../layout/breadcrumb";
+  import BreadCrumb from "../../layout/breadcrumb"; 
 import { BaseApi } from "../../../context/BaseApi";
   import { useNavigate } from "react-router-dom";
 
@@ -21,7 +22,7 @@ import { BaseApi } from "../../../context/BaseApi";
     const [httpRoute, setHttpRoute] = useState<string[] | null>([]);
     const [payload,setPayload] = useState({});
     const nav = useNavigate();
-    
+    const toast = useToast();
     const toastLoagin = (title:string,message:string,returncode:number,status: "info" | "warning" | "success" | "error" | "loading" | undefined) => {
       toast({
         title: title,
@@ -57,180 +58,176 @@ import { BaseApi } from "../../../context/BaseApi";
       }
     }
 
-    return (
-      <Box w="100%" h="100vh" bg="#F2F2F2" fontFamily="Arial, sans-serif">
-        <Box
-          py={6}
-          textAlign="center"
-          bg="#EB2937"
-          color="white"
-          borderBottomRadius="15px"
-        >
-          <Heading as="h3" size="xl">
-            Cadastrar novo produto
-          </Heading>
-        </Box>
-        <BreadCrumb items={httpRoute} />
-        <Box
-          w="90%"
-          mx="auto"
-          bg="white"
-          borderRadius="15px"
-          boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)"
-          p={8}
-          mt={8}
-        >
-          <VStack align="stretch" spacing={6}>
-            <FormControl>
-              <FormLabel htmlFor="nome" mb="1" color="#34495e">
-                Nome
-              </FormLabel>
-              <Input
-                id="nome"
-                name="Name"
-                onChange={handleInputChange}
-                type="text"
-                bg="white"
-                borderColor="#EB2937"
-                _hover={{ borderColor: "#EB2937" }}
-              />
-            </FormControl>
-  
-            <Flex justify="space-between">
-              <FormControl flex="1" mr={2}>
-                <FormLabel htmlFor="tag" mb="1" color="#34495e">
-                  Tag
-                </FormLabel>
-                <Input
-                  id="tag"
-                  type="text"
-                  name="Tag"
-                  onChange={handleInputChange}
-                  bg="white"
-                  borderColor="#EB2937"
-                  _hover={{ borderColor: "#EB2937" }}
-                />
-              </FormControl>
-  
-              <FormControl flex="2" ml={2}>
-                <FormLabel htmlFor="type" mb="1" color="#34495e">
-                  Tipo
-                </FormLabel>
-                <Input
-                  id="type"
-                  type="text"
-                  name="Type"
-                  onChange={handleInputChange}
-                  bg="white"
-                  borderColor="#EB2937"
-                  _hover={{ borderColor: "#EB2937" }}
-                />
-              </FormControl>
-            </Flex>
-  
-            <FormControl>
-              <FormLabel htmlFor="Quantity" mb="1" color="#34495e">
-                Quantidade
-              </FormLabel>
-              <Input
-                id="Quantity"
-                type="number"
-                name="Quantity"
-                onChange={handleInputChange}
-                bg="white"
-                borderColor="#EB2937"
-                _hover={{ borderColor: "#EB2937" }}
-              />
-            </FormControl>
-  
-            <FormControl>
-              <FormLabel htmlFor="Description" mb="1" color="#34495e">
-                Descrição
-              </FormLabel>
-              <Input
-                id="Description"
-                type="text"
-                name="Description"
-                onChange={handleInputChange}
-                bg="white"
-                borderColor="#EB2937"
-                _hover={{ borderColor: "#EB2937" }}
-              />
-            </FormControl>
-  
-            <FormControl>
-              <FormLabel htmlFor="Value" mb="1" color="#34495e">
-                Valor
-              </FormLabel>
-              <Input
-                id="Value"
-                type="number"
-                name="Value"
-                onChange={handleInputChange}
-                bg="white"
-                borderColor="#EB2937"
-                _hover={{ borderColor: "#EB2937" }}
-              />
-            </FormControl>
-  
-            <FormControl>
-              <FormLabel htmlFor="photo" mb="1" color="#34495e">
-                Foto
-              </FormLabel>
-              <InputGroup>
-                <Input type="file" id="photo" accept="image/*" display="none" />
-                <label htmlFor="photo">
-                  <Box
-                    as="span"
-                    cursor="pointer"
-                    borderRadius="md"
-                    borderWidth="1px"
-                    borderColor="#EB2937"
-                    p={2}
-                    display="flex"
-                    alignItems="center"
-                    _hover={{ borderColor: "#EB2937" }}
-                  >
-                    Selecionar Foto
-                  </Box>
-                </label>
-                <InputRightElement width="6rem">
-                  <Box
-                    w="100%"
-                    h="100%"
-                    borderRadius="md"
-                    borderWidth="2px"
-                    borderColor="#EB2937"
-                    overflow="hidden"
-                  >
-                    <Image
-                      src="#"
-                      alt="Preview"
-                      w="100%"
-                      h="100%"
-                      objectFit="cover"
-                    />
-                  </Box>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-  
-            <Flex justify="space-between">
-              <Button onClick={submitPayload} colorScheme="red" w="48%">
-                Cadastrar
-              </Button>
-              <Button onClick={() => nav('painel/list_product')} colorScheme="whatsapp" w="48%">
-                Voltar
-              </Button>
-            </Flex>
-          </VStack>
-        </Box>
-      </Box>
-    );
-  };
-  
-  export default CreateProduct;
 
-function toast(arg0: { title: string; description: string; status: "info" | "warning" | "success" | "error" | "loading" | undefined; duration: number; position: string; isClosable: boolean; }) {
-  throw new Error("Function not implemented.");
+return (
+  <Box w="100%" h="100vh" bg="#F2F2F2" fontFamily="Arial, sans-serif">
+    <Box
+      py={6}
+      textAlign="center"
+      bg="#EB2937"
+      color="white"
+      borderBottomRadius="15px"
+    >
+      <Heading as="h3" size="xl">
+        Cadastrar novo produto
+      </Heading>
+    </Box>
+    <BreadCrumb items={httpRoute} />
+    <Box
+      w="90%"
+      mx="auto"
+      bg="white"
+      borderRadius="15px"
+      boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)"
+      p={8}
+      mt={8}
+    >
+      <VStack align="stretch" spacing={6}>
+        <FormControl>
+          <FormLabel htmlFor="nome" mb="1" color="#34495e">
+            Nome
+          </FormLabel>
+          <Input
+            id="nome"
+            name="Name"
+            onChange={handleInputChange}
+            type="text"
+            bg="white"
+            border="1px solid #ccc"
+            _hover={{ borderColor: "#aaa", boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)" }}
+          />
+        </FormControl>
+
+        <Flex justify="space-between">
+          <FormControl flex="1" mr={2}>
+            <FormLabel htmlFor="tag" mb="1" color="#34495e">
+              Tag
+            </FormLabel>
+            <Input
+              id="tag"
+              type="text"
+              name="Tag"
+              onChange={handleInputChange}
+              bg="white"
+              border="1px solid #ccc"
+              _hover={{ borderColor: "#aaa", boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)" }}
+            />
+          </FormControl>
+
+          <FormControl flex="2" ml={2}>
+            <FormLabel htmlFor="type" mb="1" color="#34495e">
+              Tipo
+            </FormLabel>
+            <Input
+              id="type"
+              type="text"
+              name="Type"
+              onChange={handleInputChange}
+              bg="white"
+              border="1px solid #ccc"
+              _hover={{ borderColor: "#aaa", boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)" }}
+            />
+          </FormControl>
+        </Flex>
+
+        <FormControl>
+          <FormLabel htmlFor="Quantity" mb="1" color="#34495e">
+            Quantidade
+          </FormLabel>
+          <Input
+            id="Quantity"
+            type="number"
+            name="Quantity"
+            onChange={handleInputChange}
+            bg="white"
+            border="1px solid #ccc"
+            _hover={{ borderColor: "#aaa", boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)" }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="Description" mb="1" color="#34495e">
+            Descrição
+          </FormLabel>
+          <Input
+            id="Description"
+            type="text"
+            name="Description"
+            onChange={handleInputChange}
+            bg="white"
+            border="1px solid #ccc"
+            _hover={{ borderColor: "#aaa", boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)" }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="Value" mb="1" color="#34495e">
+            Valor
+          </FormLabel>
+          <Input
+            id="Value"
+            type="number"
+            name="Value"
+            onChange={handleInputChange}
+            bg="white"
+            border="1px solid #ccc"
+            _hover={{ borderColor: "#aaa", boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)" }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="photo" mb="1" color="#34495e">
+            Foto
+          </FormLabel>
+          <InputGroup>
+            <Input type="file" id="photo" accept="image/*" display="none" />
+            <label htmlFor="photo">
+              <Box
+                as="span"
+                cursor="pointer"
+                borderRadius="md"
+                borderWidth="1px"
+                borderColor="#EB2937"
+                p={2}
+                display="flex"
+                alignItems="center"
+                _hover={{ borderColor: "#EB2937" }}
+              >
+                Selecionar Foto
+              </Box>
+            </label>
+            <InputRightElement width="6rem">
+              <Box
+                w="100%"
+                h="100%"
+                borderRadius="md"
+                borderWidth="2px"
+                borderColor="#EB2937"
+                overflow="hidden"
+              >
+                <Image
+                  src="#"
+                  alt="Preview"
+                  w="100%"
+                  h="100%"
+                  objectFit="cover"
+                />
+              </Box>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+
+        <Flex justify="space-between">
+          <Button onClick={submitPayload} colorScheme="red" w="48%" bg="#EB2937" color="white" transition="background-color 0.3s, color 0.3s">
+            Cadastrar
+          </Button>
+          <Button onClick={() => nav('painel/list_product')} colorScheme="whatsapp" w="48%" bg="#25D366" color="white" transition="background-color 0.3s, color 0.3s">
+            Voltar
+          </Button>
+        </Flex>
+      </VStack>
+    </Box>
+  </Box>
+)
 }
+  export default CreateProduct;
